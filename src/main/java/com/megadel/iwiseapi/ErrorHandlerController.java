f007f4461eba1851c39c680e31cb007ba26ef2a6
awesomeKelly04 <emmanueloboh04@gmail.com>
2020-04-23 10:00:15 +0100

    Included thymeleaf

---
+++ src/main/java/com/megadel/iwiseapi/ErrorHandlerController.java
@@ -0,0 +1,18 @@
+//package com.megadel.iwiseapi;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ErrorHandlerController implements ErrorController {
+//
+//    @Override
+//    @RequestMapping("/error")
+//    @ResponseBody
+//    public String getErrorPath() {
+//        return "<center><h1>Something went wrong</h1><br><h2>Please contact website admin</h2>\n" +
+//                "<br><a href=\"/\">Home</a></center>";
+//    }
+//}
