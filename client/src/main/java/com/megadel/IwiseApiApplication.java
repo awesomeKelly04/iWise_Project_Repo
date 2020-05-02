921c7b4dd81fd98d8cc8c3cdcd534394e8ec966e
awesomeKelly04 <emmanueloboh04@gmail.com>
2020-05-02 13:31:16 +0100

    Project restructured in modules

--- client/src/main/java/com/megadel/IwiseApiApplication.java
+++ client/src/main/java/com/megadel/IwiseApiApplication.java
@@ -1,8 +1,10 @@
 package com.megadel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
--- client/src/main/java/com/megadel/controller/ErrorHandlerController.java
+++ client/src/main/java/com/megadel/controller/ErrorHandlerController.java
@@ -1,22 +1,22 @@
-package com.megadel.controller;
-
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ErrorHandlerController implements ErrorController{
-
-    @Override
-    @RequestMapping("/error")
-    @ResponseBody
-    public String getErrorPath() {
-        return "<center>" +
-                "<h1>Something went wrong</h1>" +
-                "<hr><br>" +
-                "<div><h5>Go to <a href='https://iwise-api.herokuapp.com/iwise-api/'>iWise API</a> home page<h5>" +
-                "</div>" +
-                "</center>";
-    }
-}
+//package com.megadel.controller;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ErrorHandlerController implements ErrorController{
+//
+//    @Override
+//    @RequestMapping("/error")
+//    @ResponseBody
+//    public String getErrorPath() {
+//        return "<center>" +
+//                "<h1>Something went wrong</h1>" +
+//                "<hr><br>" +
+//                "<div><h5>Go to <a href='https://iwise-api.herokuapp.com/iwise-api/'>iWise API</a> home page<h5>" +
+//                "</div>" +
+//                "</center>";
+//    }
+//}
