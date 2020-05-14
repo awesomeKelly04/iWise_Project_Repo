0d538c5e611e69ecb908cacb0f29c0c1d136385e
awesome <emmanueloboh04@gmail.com>
2020-05-14 23:59:14 +0100

    Update api/auth/signup

--- src/main/java/com/megadel/client/controller/AuthController.java
+++ src/main/java/com/megadel/client/controller/AuthController.java
@@ -82,7 +82,7 @@ public class AuthController {
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-        Role userRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
                 .orElseThrow(() -> new AppException("User Role not set."));
 
         user.setRoles(Collections.singleton(userRole));
--- src/main/java/com/megadel/service/SignUpRequest.java
+++ src/main/java/com/megadel/service/SignUpRequest.java
@@ -51,4 +51,5 @@ public class SignUpRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
